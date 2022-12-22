f0583c8dfbcfcdbde58134f8ea2f6af120d0a284
Olga <princessca.olga@icloud.com>
2022-12-22 14:59:48 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,22 @@
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и выводит название этого дня недели
+# number = int(input('Введите число дня недели: '))
+if number == 1:
+    print('Понедельник')
+if number == 2:
+    print('Вторник')
+if number == 3:
+    print('Среда')    
+if number == 4:
+    print('Четверг')   
+if number == 5:
+    print('Пятница')  
+if number == 6:
+    print('Суббота')   
+if number == 7:
+    print('Воскресенье')               
+elif number > 7 or number < 1:
+    print('Нет такого дня')
+      
+
+
+
